467f130d9071ba49ee16bc7567265754b6ab6211
Gavy Aggarwal <gavyaggarwal@gmail.com>
2016-09-13 18:57:40 -0400

    force rebuild

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.send('Hello World! 2');
 });
 
 // error handling
